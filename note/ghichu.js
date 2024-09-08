d2162a10704634ea590cc900bd758e1afed3facb
Nguyen Dong <dongcoi14122003@gmail.com>
2024-09-09 01:08:07 +0700

    react-sprinners + protected page

---
+++ note/ghichu.js
@@ -0,0 +1,8 @@
+/*
+    # Thư viện react-spinners:
+    - Thư viện này giúp tạo ra các animation loading
+    - Tài liệu: https://www.npmjs.com/package/react-spinners?activeTab=readme
+    => Xem các component của thư viện thì bấm vào Demo page của link trên -> 
+        https://www.davidhu.io/react-spinners/
+        https://www.davidhu.io/react-spinners/storybook/?path=/docs/barloader--docs
+*/
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import {
   createBrowserRouter,
-  Outlet,
   RouterProvider,
 } from "react-router-dom";
 import BookPage from './pages/book';
 import ContactPage from './pages/contact';
 import LoginPage from './pages/login';
+import { Outlet } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import RegisterPage from './pages/register';
 import { callFetchAccount } from './services/api';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { doGetAccountAction } from './redux/account/accountSlice';
+import Loading from './components/Loading';
+import NotFound from './components/NotFound';
+import AdminPage from './pages/admin';
+import ProtectedRoute from './components/ProtectedRoute';
 
 
 const Layout = () => {
@@ -28,11 +32,15 @@ const Layout = () => {
 
 export default function App() {
   const dispatch = useDispatch();
+  const isAuthenticated = useSelector(state => state.account.isAuthenticated)
 
   const getAccount = async () => {
+    if (window.location.pathname === '/login'
+    )
+      return;
     const res = await callFetchAccount();
     if (res && res.data) {
-      dispatch(doGetAccountAction(res.data))  // lưu thông tin user vào redux mỗi khi load lại trang
+      dispatch(doGetAccountAction(res.data))
     }
   }
 
@@ -44,11 +52,33 @@ export default function App() {
     {
       path: "/",
       element: <Layout />,
-      errorElement: <div>404 not found</div>,
+      errorElement: <NotFound />,
       children: [
         { index: true, element: <Home /> },
         {
           path: "contact",
+          element: <ContactPage />,
+        },
+        {
+          path: "book",
+          element: <BookPage />,
+        },
+      ],
+    },
+
+    {
+      path: "/admin",
+      element: <Layout />,
+      errorElement: <NotFound />,
+      children: [
+        {
+          index: true, element:
+            <ProtectedRoute>
+              <AdminPage />
+            </ProtectedRoute>
+        },
+        {
+          path: "user",
           element: <ContactPage />,
         },
         {
@@ -68,13 +98,18 @@ export default function App() {
       path: "/register",
       element: <RegisterPage />,
     },
-
   ]);
 
 
   return (
     <>
-      <RouterProvider router={router} />
+      {isAuthenticated === true
+        || window.location.pathname === '/login'
+        ?
+        <RouterProvider router={router} />
+        :
+        <Loading />
+      }
     </>
   )
 }
