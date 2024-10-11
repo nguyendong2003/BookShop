ee5da39eb64553b06c65d39b101e79ad202a5a72
Nguyen Dong <dongcoi14122003@gmail.com>
2024-10-12 01:53:30 +0700

    Design Modal Upload File (Drag & Drop)

--- src/components/Admin/User/UserTable.jsx
+++ src/components/Admin/User/UserTable.jsx
@@ -7,6 +7,7 @@ import UserModalCreate from './UserModalCreate';
 import UserViewDetail from './UserViewDetail';
 import moment from 'moment/moment';
 import { FORMAT_DATE_DISPLAY } from '../../../utils/constant';
+import UserImport from './data/UserImport';
 
 const UserTable = () => {
     const [listUser, setListUser] = useState([]);
@@ -21,6 +22,10 @@ const UserTable = () => {
     const [openModalCreate, setOpenModalCreate] = useState(false);
     const [openViewDetail, setOpenViewDetail] = useState(false);
     const [dataViewDetail, setDataViewDetail] = useState(null);
+
+    const [openModalImport, setOpenModalImport] = useState(false);
+
+
 
     useEffect(() => {
         fetchUser();
@@ -146,6 +151,7 @@ const UserTable = () => {
                     <Button
                         icon={<CloudUploadOutlined />}
                         type="primary"
+                        onClick={() => setOpenModalImport(true)}
                     >Import</Button>
 
                     <Button
@@ -216,6 +222,12 @@ const UserTable = () => {
                 dataViewDetail={dataViewDetail}
                 setDataViewDetail={setDataViewDetail}
             />
+
+            <UserImport
+                openModalImport={openModalImport}
+                setOpenModalImport={setOpenModalImport}
+            />
+
         </>
     )
 }
