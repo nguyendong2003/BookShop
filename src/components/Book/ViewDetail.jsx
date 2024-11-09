dbc8212db33db40140e14f1a6eb1cc89551fcae0
Nguyen Dong <dongcoi14122003@gmail.com>
2024-11-10 01:45:28 +0700

    add skeleton in view book detail page

--- src/components/Book/ViewDetail.jsx
+++ src/components/Book/ViewDetail.jsx
@@ -1,8 +1,11 @@
-import { Row, Col, Rate, Divider } from 'antd';
+import { Row, Col, Rate, Divider, Button } from 'antd';
 import './book.scss';
 import ImageGallery from 'react-image-gallery';
 import { useRef, useState } from 'react';
 import ModalGallery from './ModalGallery';
+import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
+import { BsCartPlus } from 'react-icons/bs';
+import BookLoader from './BookLoader';
 
 const ViewDetail = (props) => {
 
@@ -82,22 +85,10 @@ const ViewDetail = (props) => {
 
     return (
         <div style={{ background: '#efefef', padding: "20px 0" }}>
-            <div className='view-detail-book' style={{ maxWidth: 1440, margin: '0 auto' }}>
-                <Row gutter={[20, 20]}>
-                    <Col md={10} sm={0} xs={0}>
-                        <ImageGallery
-                            ref={refGallery}
-                            items={images}
-                            showPlayButton={false} //hide play button
-                            showFullscreenButton={false} //hide fullscreen button
-                            renderLeftNav={() => <></>} //left arrow === <> </>
-                            renderRightNav={() => <></>}//right arrow === <> </>
-                            slideOnThumbnailOver={true}  //onHover => auto scroll images
-                            onClick={() => handleOnClickImage()}
-                        />
-                    </Col>
-                    <Col md={14} sm={24}>
-                        <Col md={0} sm={24} xs={24}>
+            <div className='view-detail-book' style={{ maxWidth: 1440, margin: '0 auto', minHeight: "calc(100vh - 150px)" }}>
+                <div style={{ padding: "20px", background: '#fff', borderRadius: 5 }}>
+                    {/* <Row gutter={[20, 20]}>
+                        <Col md={10} sm={0} xs={0}>
                             <ImageGallery
                                 ref={refGallery}
                                 items={images}
@@ -105,45 +96,62 @@ const ViewDetail = (props) => {
                                 showFullscreenButton={false} //hide fullscreen button
                                 renderLeftNav={() => <></>} //left arrow === <> </>
                                 renderRightNav={() => <></>}//right arrow === <> </>
-                                showThumbnails={false}
+                                slideOnThumbnailOver={true}  //onHover => auto scroll images
+                                onClick={() => handleOnClickImage()}
                             />
                         </Col>
-                        <Col span={24}>
-                            <div className='author'>Tác giả: Jo Hemmings</div>
-                            <div className='title'>How Psychology Works - Hiểu Hết Về Tâm Lý Học</div>
-                            <div className='rating'>
-                                <Rate value={5} disabled style={{ color: '#ffce3d', fontSize: 12 }} />
-                                <Divider type="vertical" />
-                                <span className='sold'>Đã bán 6969</span>
-                            </div>
-                            <div className='price'>
-                                <span>
-                                    {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(696966666)}
-                                </span>
-                            </div>
-                            <div className='delivery'>
-                                <div>
-                                    <span>Vận chuyển</span>
-                                    <span>Miễn phí vận chuyển</span>
+                        <Col md={14} sm={24}>
+                            <Col md={0} sm={24} xs={24}>
+                                <ImageGallery
+                                    ref={refGallery}
+                                    items={images}
+                                    showPlayButton={false} //hide play button
+                                    showFullscreenButton={false} //hide fullscreen button
+                                    renderLeftNav={() => <></>} //left arrow === <> </>
+                                    renderRightNav={() => <></>}//right arrow === <> </>
+                                    showThumbnails={false}
+                                />
+                            </Col>
+                            <Col span={24}>
+                                <div className='author'>Tác giả: <a href='#'>Jo Hemmings</a> </div>
+                                <div className='title'>How Psychology Works - Hiểu Hết Về Tâm Lý Học</div>
+                                <div className='rating'>
+                                    <Rate value={5} disabled style={{ color: '#ffce3d', fontSize: 12 }} />
+                                    <span className='sold'>
+                                        <Divider type="vertical" />
+                                        Đã bán 6969</span>
                                 </div>
-                            </div>
-                            <div className='quantity'>
-                                <div>
-                                    <span>Số lượng</span>
-                                    <span>
-                                        <button>-</button>
-                                        <input />
-                                        <button>+</button>
+                                <div className='price'>
+                                    <span className='currency'>
+                                        {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(696966666)}
                                     </span>
                                 </div>
-                            </div>
-                            <div>
-                                <button>Thêm vào giỏ hàng</button>
-                                <button>Mua ngay</button>
-                            </div>
+                                <div className='delivery'>
+                                    <div>
+                                        <span className='left'>Vận chuyển</span>
+                                        <span className='right'>Miễn phí vận chuyển</span>
+                                    </div>
+                                </div>
+                                <div className='quantity'>
+                                    <span className='left'>Số lượng</span>
+                                    <span className='right'>
+                                        <button ><MinusOutlined /></button>
+                                        <input defaultValue={1} />
+                                        <button><PlusOutlined /></button>
+                                    </span>
+                                </div>
+                                <div className='buy'>
+                                    <button className='cart'>
+                                        <BsCartPlus className='icon-cart' />
+                                        <span>Thêm vào giỏ hàng</span>
+                                    </button>
+                                    <button className='now'>Mua ngay</button>
+                                </div>
+                            </Col>
                         </Col>
-                    </Col>
-                </Row>
+                    </Row> */}
+                    <BookLoader />
+                </div>
             </div>
             <ModalGallery
                 isOpen={isOpenModalGallery}
